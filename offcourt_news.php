97700e0d296022b61f9697284c77531b83c5eaea
Paulon Zervoulakus <paper.paulon@gmail.com>
2014-02-27 07:12:11 -0800

    added disqus again in offcourt news

--- offcourt_news.php
+++ offcourt_news.php
@@ -167,8 +167,21 @@ include('layouts/header.php');
                   <!-- AddThis Button END -->
                </div>
             </div>
-
-            <div class="fb-comments" data-href="<?php echo $base; ?>off-court-news/<?php echo $off_id; ?>/<?php  echo seoUrl($offcourt_title); ?>" data-num-posts="2" data-width="610"></div>
+			<div id="disqus_thread" style='padding:10px;'></div>
+			<script type="text/javascript">
+				/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+				var disqus_shortname = 'ph-nba-com'; // required: replace example with your forum shortname					
+				var disqus_url = '<?php echo $base; ?>off-court-news/<?php echo $off_id; ?>/<?php  echo seoUrl($offcourt_title); ?>';
+				/* * * DON'T EDIT BELOW THIS LINE * * */
+				(function() {
+					var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+					dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+					(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+				})();
+			</script>
+			<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+			<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+            
 <?php
                   }
                  else
