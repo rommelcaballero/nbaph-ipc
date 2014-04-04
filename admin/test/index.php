78d5bd6e4501cb8f1b9f0703f63e7dbd50ca9395
xplay-paulon <paulon.zervoulakus@xplay.ph>
2014-04-04 18:20:05 +0800

    .

--- admin/test/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if(isset($_POST['btnSubmit'])){
-		echo $_POST['txtName'];
-	}
-?>
-<form method="post">
-	<input type="text" name="txtName">
-	<input type="submit" name="btnSubmit">
-</form>
