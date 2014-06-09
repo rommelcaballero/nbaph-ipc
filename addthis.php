2a1ee0c1bab50769dcba5a1293583ca662fc7ce1
xplay-paulon <paulon.zervoulakus@xplay.ph>
2014-06-09 07:13:44 +0000

    added

---
+++ addthis.php
@@ -0,0 +1,15 @@
+						<div class="addthis_div" >
+                          				<div class="addthis_position" style='padding:0 20px;'>
+                                                        <div class="addthis_toolbox addthis_default_style ">
+                                                        <a class="addthis_button_preferred_1"></a>
+                                                        <a class="addthis_button_preferred_2"></a>
+                                                        <a class="addthis_button_preferred_3"></a>
+                                                        <a class="addthis_button_preferred_4"></a>
+                                                        <a class="addthis_button_compact"></a>
+                                                        <a class="addthis_counter addthis_bubble_style"></a>
+                                                        </div>
+                                                        <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+                                                        <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-503c81d36918b206"></script>
+
+                                                        </div>
+                                                </div>
