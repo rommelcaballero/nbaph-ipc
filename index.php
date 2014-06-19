4c9fd3f703a5b9dbe137dbcaa8ca04df71cb148a
xplay-paulon <paulon.zervoulakus@xplay.ph>
2014-06-19 09:02:05 +0000

    add google ad

--- index.php
+++ index.php
@@ -110,6 +110,28 @@ if($www!=="ph"){
 
 ?>
 
+
+     <script type='text/javascript'>
+(function() {
+var useSSL = 'https:' == document.location.protocol;
+var src = (useSSL ? 'https:' : 'http:') +
+'//www.googletagservices.com/tag/js/gpt.js';
+document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+})();
+</script>
+
+<script type='text/javascript'>
+googletag.defineSlot('/7741304/Interstitial_NBA_Homepage', [1, 1], 'div-gpt-ad-1401002731227-0').addService(googletag.pubads());
+googletag.pubads().enableSyncRendering();
+googletag.enableServices();
+</script>
+
+<!-- Interstitial_NBA_Homepage -->
+<div id='div-gpt-ad-1401002731227-0' style='width:1px; height:1px;'>
+<script type='text/javascript'>
+googletag.display('div-gpt-ad-1401002731227-0');
+</script>
+</div>
 	
 	</body>
 
