afc6eea85e083143772daf5bf98a2ad17a9e7ae6
xplay-paulon <paulon.zervoulakus@xplay.ph>
2014-08-30 09:43:42 +0000

    nba2k tournament

---
+++ nba2k14tournament-learnmore-content.php
@@ -0,0 +1,63 @@
+<div style="left:33px;width:935px;font-size: 14px; color: #444;font-family:arial,helvetica;">
+
+REGISTRATION MECHANICS FOR HIP-HOP DANCE WORKSHOP
+<br><br>
+Registration Day (September 6, 2014)
+<br><br>
+Sign up at our booth located at the left side of the Glorietta Main Activity Center to be able to join the Hip-Hop Dance Workshop. The registration booth will be open from 10:00 am until 9:00 PM. Registration will start at promptly 10:00 am.
+<br><br>
+The Hip-Hop Dance Workshop will be held at the Glorietta Main Activity Center on September 12, 2014.  The Hip-Hop Dance Workshop will be instructed by members of The Philippine All Stars.
+<br><br>
+There will be two divisions for the Hip-Hop Dance workshops:
+<br><br>
+&nbsp;&nbsp;&nbsp;&nbsp;1.	For Teens (aged 17 and below).  20 participants plus 10 wait listed individuals. <br>
+&nbsp;&nbsp;&nbsp;&nbsp;2.	For Adults (18 and above). 20 participants plus 10 wait listed individuals. 
+<br><br>
+
+In order to register interested individuals will need to do the following: 
+<br><br>
+&nbsp;&nbsp;&nbsp;&nbsp;1.	Must fill-out necessary details in the registration form.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;2.	All information given must be true and correct.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;3.	Adults must present a valid ID showing date of their birth.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;4.	For minors ages 17 and below they will need to bring a signed letter of consent from parents or a guardian and a valid ID.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;5.	Registered participants and the wait listed individuals must sign the waiver form. <br>
+<br>
+The 20 slots for each division will be given to the first 20 people who register. The 10 wait listed slots will be given to the next ten people who register after the 20 slots are filled.
+<br><br>
+The 40 registered participants will be given a NBA shirt and NBA sling bag on September 6, 2014 that they will need to wear and use on September 12, 2014.
+<br><br>
+Event Day: Hip-Hop Dance Workshop (September 12, 2014) 
+<br><br>
+Participants must be present at the area 1 hour before the start of the workshop.
+<br>
+<ul>
+<li>Adults Workshop starts at 1:30 PM. Call time for the participants is at 12:30 PM.</li>
+<li>Teens Workshop starts at 5:00 PM. Call time for the participants is at 4:00 PM.</li>  
+</ul>
+<br>
+* Each Hip-Hop Dance Workshop will last 1 hour and 30 minutes with a possible 30 minutes dance battle. 
+<br><br>
+In order to participate in the Hip-Hop Dance Workshop on September 12, 2014 the registered individuals must do the following:
+<br><br>
+&nbsp;&nbsp;&nbsp;&nbsp;1.	Must bring a valid ID.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;2.	Wear their NBA Shirt.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;3.	Bring and use their NBA Sling Bag.  
+<br><br>
+The 40 registered individuals will also get the following NBA goodies after completing the Hip-Hop Dance workshop: NBA Tumbler, NBA Face Towel and other NBA goodies. 
+<br><br>
+Selected people from the 40 participants will also get a chance to win exciting prizes if the person gets hand-picked by the Philippine All Stars to be part of Dance Battle.
+<br><br>
+The top 2 winners of the dance battles will win the following: 
+<br>
+<ul>
+<li>1st place Hip-Hop Dancer: Acer Tablet, 5,000 worth of GC’s and Meet & Greet with NBA player plus photo opportunity.</li>
+<li>2nd place Hip-Hop Dancer: 2,500 worth of GC's and Meet & Greet with NBA player plus photo opportunity.</li> 
+</ul>
+<br>
+
+NOTE: REGISTRATION MECHANICS AND PRIZES ARE SUBJECT TO CHANGE WITHOUT PRIOR NOTICE.
+
+<br><br>
+</div>
+<br><br>
+<div style="margin: 0 auto;display:table;"><button>Register now</button></div>
---
+++ nba2k14tournament-learnmore.php
@@ -0,0 +1,104 @@
+<?php
+	$www = @$_GET['www'];
+	
+        $part_page = "index";
+        $beta = false;
+        include('queries/index-queries-new.php');
+
+                if($num_wall_videos > 0){
+                        $csrf = md5("nbaph-".@date("Y-m-d")."-".@$_SERVER['HTTP_X_FORWARDED_FOR']."-".@$_SERVER['REMOTE_ADDR']);
+                        $_SESSION['_csrf'] = $csrf;
+                        $up = '1';
+                }
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NBA Philippines</title>
+	<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1" /> 	
+	
+	<META http-equiv="X-UA-Compatible" content="IE=9" />
+	<meta http-equiv="Cache-control" content="public">
+	
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style-index.css">
+	<link rel="stylesheet" type="text/css" href="style-new.css">
+	
+	<link rel="stylesheet" href="jquery-ui.css" />
+	<script type="text/javascript" src="jquery-1.9.1.js"></script>
+	<script type="text/javascript" src="jquery-ui.js"></script>
+	<script type="text/javascript" src="carousel-index.js"></script>
+	
+	<?php //include('leaderboards_js.php'); ?>
+	</head>
+	<body>
+
+
+<?php
+if($up=='1' & $www==''){
+//include('wall.php');
+//$go ='';
+$start='';
+}
+
+if($www!=="ph"){
+//exit();
+}
+
+?>
+
+
+		<?php //include('layouts/popups.php'); ?>
+		<div id="wrapper">
+			<?php include('layouts/header.php'); ?>
+			<div id="main_content" > 
+				<div id='header'>
+	      	  		<div style="height: 30px"></div>
+	          		<div style="width: 958px; min-height: 20px; margin: 0 auto; text-align: center;">
+						<?php //echo $ads_list['nba_homepage_top_leaderboard'];?>
+	          		</div>    	          		
+					<div style="width: 995px;  margin: 0 auto; text-align: center; background: #fff; padding-left:5px;">
+						<!--
+						<iframe src="http://ph.global.nba.com/hpscoreboardiframe.html" align="center" frameborder="0" width="1000" height="143" scrolling="no" marginwidth="0" marginheight="0" style="text-align:center; align: center; margin: 0; padding: 0; marginwidth:0"></iframe>
+						-->
+					</div>
+				</div>
+
+<div id="containerlearnmore" style="position:relative">
+	<?php include('nba2k14tournament-learnmore-content.php');  ?>
+</div>
+
+
+<div>
+		          	<?php 
+		          	$footer_ads = $ads_list['nba_homepage_bottom_leaderboard']; 
+		          	include('layouts/links.php'); 
+		          	include('layouts/footer.php');
+		          	?>
+		          	<div class='clear'></div>
+	          	</div>
+          	</div><!-- main_content -->   
+          	    
+		</div><!-- wrapper -->		
+		<?php //include("layouts/background_ads.php"); ?>
+            
+                <?php //if(count($wall_videos) > 0) include("wall.php"); ?>  	
+<?php 
+//		if($go == '1'){
+//			if($num_wall_videos > 0) {
+  //                      	include("wall.php");
+//				$go ='';
+//				$start = '';
+  //                      }
+//		}
+
+?>
+
+
+	
+	</body>
+
+</html>
