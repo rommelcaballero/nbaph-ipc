7c724229798d01307152df3f8067ce3de6c79b2f
mobile <ricardo.donato@xplay.ph>
2014-11-11 03:33:01 +0000

    added google code on footer

--- footer.php
+++ footer.php
@@ -414,3 +414,25 @@ src="http://www.nba.com/javascriptGlobalSettings.js.html" type="text/javascript"
 <script id="_nbaScoutAnalytics" 
 src="http://z.cdn.turner.com/nba/tmpl_asset/static/nba-cms3-base/latest/js/pkgAnalyticsScout-min.js" type="text/javascript"></script>
 <!-- END: NBA analytics package snippet. -->
+
+<script type='text/javascript'>
+(function() {
+var useSSL = 'https:' == document.location.protocol;
+var src = (useSSL ? 'https:' : 'http:') +
+'//www.googletagservices.com/tag/js/gpt.js';
+document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+})();
+</script>
+
+<script type='text/javascript'>
+googletag.defineSlot('/7741304/Interstitial_NBA_Homepage', [1, 1], 'div-gpt-ad-1401002731227-0').addService(googletag.pubads());
+googletag.pubads().enableSyncRendering();
+googletag.enableServices();
+</script>
+
+<!-- Interstitial_NBA_Homepage -->
+<div id='div-gpt-ad-1401002731227-0' style='width:1px; height:1px;'>
+<script type='text/javascript'>
+googletag.display('div-gpt-ad-1401002731227-0');
+</script>
+</div>
